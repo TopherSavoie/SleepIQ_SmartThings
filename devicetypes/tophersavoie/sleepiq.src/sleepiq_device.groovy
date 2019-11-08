86d6fa581b2c029674f0f500239dc2b7f0a5738f
Chris Savoie <chris@savoi.es>
2019-11-08 14:51:06 -0600

    Update sleepiq_device.groovy

--- devicetypes/tophersavoie/sleepiq.src/sleepiq_device.groovy
+++ devicetypes/tophersavoie/sleepiq.src/sleepiq_device.groovy
@@ -12,6 +12,9 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  */
+preferences {
+	input(name: "foundation", type: "bool", title: "Adjustable Foundation", required: "true", defaultValue: false)
+}
 metadata {
 	definition (name: "Sleep IQ", namespace: "TopherSavoie", author: "TopherSavoie", cstHandler: true) {
 		capability "Switch Level"
