bd25372f99d540a8c3945b5a789c8d6f633a92da
Chris Savoie <chris@savoi.es>
2019-11-08 12:33:10 -0600

    Update sleepiq.groovy

--- smartapps/tophersavoie/sleepiq.src/sleepiq.groovy
+++ smartapps/tophersavoie/sleepiq.src/sleepiq.groovy
@@ -1,7 +1,7 @@
 /**
  *  Sleep Number
  *
- *  Copyright 2019 Tim Parsons
+ *  Copyright 2019 Topher Savoie
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
